0f604a69d833f25d122c6b0dde52d37f579e0684
Felipe Silva <frsilva.dev@gmail.com>
2025-11-06 16:12:15 +0000

    Add content to Contact

--- chicobarbearia/src/components/Contact/Contact.jsx
+++ chicobarbearia/src/components/Contact/Contact.jsx
@@ -1,9 +1,36 @@
 import './Contact.css'
-
+import maps from '../../assets/images/chico-barbearia.png'
+import pin from '../../assets/images/pin.png'
+7
 const Contact = () => {
   return (
     <section id='contact' className='contact'>
-      <h1>CONTACT</h1>
+      <div className='contact-title'>
+        <img src={pin} alt="pin" className='pin'/>
+        <h1>Onde estamos?</h1>
+      </div>      
+      <div className='info'>
+        <div className='info-details'>
+          <h2>Contactos</h2>
+          <h3>Morada:</h3>
+          <p>Rua Lucinda Simões 12E</p>
+          <p>1900-305 Lisboa</p>
+          <br />
+          <h3>Telemóvel:</h3>
+          <p>919919919</p>
+        </div>
+        <div className='maps'>
+          <a 
+            href="https://www.google.com/maps/place/Chico+Barbearia+Nudista/@38.7354405,-9.1319537,17z/data=!4m6!3m5!1s0xd1933001d18749b:0xc8d34db62afa2810!8m2!3d38.73552!4d-9.1319752!16s%2Fg%2F11mlgplpjw?entry=ttu&g_ep=EgoyMDI1MTEwMi4wIKXMDSoASAFQAw%3D%3D"
+            target="_blank"
+            rel="noopener noreferrer"
+            className='maps-link'
+          >
+            <img src={maps} alt="maps" className='maps-img'/>
+          </a>
+        </div>
+      </div>
+      
     </section>
   )
 }
